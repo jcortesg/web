06f27a1a5a11f349850d1da73d998c9aea84f86b
Jhon Cortes <jhone.cortesg@gmail.com>
2013-11-23 08:50:48 -0500

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Banko::Application.config.secret_token = '2a97fc1c1df005ab479ee2620c697dfadcbf103493d96b88f22ad300de72351c56c83055d7869fcfd986a2d20efc5e03df50aba89ea72dc666e1ac7e5413d38b'
+Banko::Application.config.secret_token = '294a5bd95fcf892d061a3647a70a4c0c75561a24bdb5a7416081dca355462cde1a153294fc433112d8b434f39366b1a51c2a8ad7a7a12e47f665fbba56be698b'
