490210925f084feafec5396707266be02e45b10d
Jhon Cortes <jhone.cortesg@gmail.com>
2013-11-23 08:48:52 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Banko::Application.config.secret_token = '2a97fc1c1df005ab479ee2620c697dfadcbf103493d96b88f22ad300de72351c56c83055d7869fcfd986a2d20efc5e03df50aba89ea72dc666e1ac7e5413d38b'
